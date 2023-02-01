10000d6462022e730b4531d92aaddde8a1a20ef8
JunLee366 <junholee366@gmail.com>
2023-02-01 15:40:26 -0500

    onclick print country name

--- geojson/src/components/MapView.js
+++ geojson/src/components/MapView.js
@@ -4,20 +4,21 @@ import L from 'leaflet';
 import 'leaflet/dist/leaflet.css';
 
 function MapView(props) {
-    const nameChange = (country) => {
-        console.log(country);
+    const nameChange = (event) => {
+        let layer = event.target;
+        console.log(layer.feature.properties.name);
     };
 
-    const onEachCountry = (country, layer) => {
-        const countryName = country.properties.name;
-        // layer.bindTooltip(country.properties.name,
-        // {permanent: true, direction:"center",className: "label"}
-        // ).openTooltip();
-        layer.bindPopup(countryName);
+    const onEachCountry = (feature, layer) => {
+        const countryName = feature.properties.name;
+        layer.bindTooltip(feature.properties.name,
+        {permanent: true, direction:"center",className: "label"}
+        ).openTooltip();
+        // layer.bindPopup(countryName);
         layer.options.fillOpacity = 0.4;
 
         layer.on({
-            click: nameChange
+            click: nameChange.bind(this)
         });
     };
 
@@ -27,7 +28,8 @@ function MapView(props) {
             <div>
             <MapContainer style={{ height: "80vh" }} zoom={2} center={[20, 100]} >
                 <GeoJSON 
-                    data={props.file.features} onEachFeature={onEachCountry}
+                    data={props.file.features} 
+                    onEachFeature={onEachCountry}
                 />
             </MapContainer>
             </div>
