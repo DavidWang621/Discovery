042be1c16936c5960288e6a1ae2f855ff08e5363
DavidWang621 <davidwang759@gmail.com>
2023-02-01 17:12:39 -0500

    cann change data but not gui

--- geojson/src/App.js
+++ geojson/src/App.js
@@ -4,6 +4,7 @@ import MapView from './components/MapView';
 function App() {
   const [fileExist, setFileExist] = useState(false);
   const [GeoJson, setGeoJson] = useState({});
+  const [state, setState] = useState(false);
 
   const handleSelectFile = (e) => {
     const reader = new FileReader();
@@ -14,11 +15,24 @@ function App() {
     setFileExist(true);
   }
 
+  const changeRegionName = (oldName, newName) =>{
+    console.log("newold", oldName, newName);
+    let temp = GeoJson;
+    temp.features.forEach((i) => {
+      if (i.properties.name === oldName) {
+        i.properties.name = newName;
+        setGeoJson(temp);
+      }
+    })
+    console.log("features", GeoJson.features);
+            setState(!state);
+  }
+
   return (
     <div className="App">
       <div>Imput geo.json File</div>
       <input type="file" accept="geo.json" onChange={handleSelectFile}/>
-      {fileExist ? <MapView file={GeoJson}/> : <></>}
+      {fileExist ? <MapView file={GeoJson} changeName={changeRegionName}/> : <></>}
     </div>
   );
 }
--- geojson/src/components/MapView.js
+++ geojson/src/components/MapView.js
@@ -4,21 +4,27 @@ import L from 'leaflet';
 import 'leaflet/dist/leaflet.css';
 
 function MapView(props) {
+    const [isPopup, setPopup] = useState(false);
+
     const nameChange = (event) => {
         let layer = event.target;
         console.log(layer.feature.properties.name);
+
+        let newCountry = prompt("Input new region name:", layer.feature.properties.name);
+        props.changeName(layer.feature.properties.name, newCountry);
+        layer.feature.properties.name = newCountry;
     };
 
     const onEachCountry = (feature, layer) => {
         const countryName = feature.properties.name;
-        layer.bindTooltip(feature.properties.name,
+        layer.bindTooltip(layer.feature.properties.name,
         {permanent: true, direction:"center",className: "label"}
         ).openTooltip();
         // layer.bindPopup(countryName);
         layer.options.fillOpacity = 0.4;
 
         layer.on({
-            click: nameChange.bind(this)
+            dblclick: nameChange.bind(this)
         });
     };
 
@@ -26,12 +32,12 @@ function MapView(props) {
         <div>
             {props.file.features ?
             <div>
-            <MapContainer style={{ height: "80vh" }} zoom={2} center={[20, 100]} >
-                <GeoJSON 
-                    data={props.file.features} 
-                    onEachFeature={onEachCountry}
-                />
-            </MapContainer>
+                <MapContainer style={{ height: "80vh" }} zoom={2} center={[20, 100]} >
+                    <GeoJSON 
+                        data={props.file.features} 
+                        onEachFeature={onEachCountry}
+                    />
+                </MapContainer>
             </div>
             : 
             <></>
