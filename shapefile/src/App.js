1cef3fd010a9e52a4c4f237c942bbddb254fe1bd
daoqingao <daoqingao@gmail.com --git config --global user.email daoqingao@gmail.com>
2023-02-02 23:40:46 -0500

    broken shapefile upload

--- shapefile/src/App.js
+++ shapefile/src/App.js
@@ -1,24 +1,88 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from 'react';
+import MapView from './components/MapView';
+import * as shapefile from "shapefile";
 
 function App() {
+
+  const [fileExist, setFileExist] = useState(false);
+  const [GeoJson, setGeoJson] = useState({});
+  const [state, setState] = useState(false);
+
+  const [data, setData] = React.useState([]);
+
+
+
+
+  const readShapefile = (e) => {
+    //can only upload shp file but not zip file for now
+    let res = [];
+    shapefile
+        .open(
+            e,
+            //"https://cdn.rawgit.com/mbostock/shapefile/master/test/points.shp",
+            null
+        ).then(function (e){
+      e.read().then(
+          function next(result){
+            if(result.value)
+              res.push(result.value)
+            if(!result.done){
+              e.read().then(next)
+            }
+          })
+    })
+    console.log("finish shpfile")
+    console.log(res)
+    return res
+  }
+
+
+
+  const handleSelectFile = (e) => {
+    {
+
+      //this is for geojson
+      // const reader = new FileReader();
+      // setGeoJson({});
+      // reader.readAsText(e.target.files[0]);
+      // reader.onload = e => {
+      //   setGeoJson(JSON.parse(e.target.result));
+      // }
+      // setFileExist(true);
+      // console.log(JSON.stringify(GeoJson))
+
+
+      //this is for shapefile
+      const reader = new FileReader();
+      setGeoJson({});
+      reader.readAsArrayBuffer(e.target.files[0])
+      reader.onload = async e => {
+        await readShapefile(reader.result)
+      }
+      setFileExist(true);
+      console.log("shp file read")
+  }
+  }
+
+  const changeRegionName = (oldName, newName) =>{
+    console.log("newold", oldName, newName);
+    let temp = GeoJson;
+    temp.features.forEach((i) => {
+      if (i.properties.name === oldName) {
+        i.properties.name = newName;
+        setGeoJson(temp);
+      }
+    })
+    console.log("features", GeoJson.features);
+    setState(!state);
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+      <div className="App">
+        <div>Imput geo.json File</div>
+        <input type="file" accept="geo.json" onChange={handleSelectFile}/>
+        {fileExist ? <MapView file={GeoJson} changeName={changeRegionName}/> : <></>}
+      </div>
   );
 }
 
---
+++ shapefile/src/components/MapView.js
@@ -0,0 +1,51 @@
+import React, { useState, useEffect } from 'react';
+import { MapContainer, TileLayer, useMap, GeoJSON } from 'react-leaflet';
+import L from 'leaflet';
+import 'leaflet/dist/leaflet.css';
+
+function MapView(props) {
+    const [isPopup, setPopup] = useState(false);
+
+    const nameChange = (event) => {
+        let layer = event.target;
+        console.log(layer.feature.properties.name);
+
+        let newCountry = prompt("Input new region name:", layer.feature.properties.name);
+        props.changeName(layer.feature.properties.name, newCountry);
+        layer.bindTooltip(layer.feature.properties.name,
+            {permanent: true, direction:"center",className: "label"}
+            ).openTooltip();
+    };
+
+    const onEachCountry = (feature, layer) => {
+        const countryName = feature.properties.name;
+        layer.bindTooltip(layer.feature.properties.name,
+        {permanent: true, direction:"center",className: "label"}
+        ).openTooltip();
+        // layer.bindPopup(countryName);
+        layer.options.fillOpacity = 0.4;
+
+        layer.on({
+            dblclick: nameChange.bind(this)
+        });
+    };
+
+    return (
+        <div>
+            {props.file.features ?
+            <div>
+                <MapContainer style={{ height: "80vh" }} zoom={2} center={[20, 100]} >
+                    <GeoJSON 
+                        data={props.file.features} 
+                        onEachFeature={onEachCountry}
+                    />
+                </MapContainer>
+            </div>
+            : 
+            <></>
+            }
+        </div>
+    )
+}
+
+export default MapView;
