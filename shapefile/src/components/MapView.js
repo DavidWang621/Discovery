a32bee97104b9e4ec36d1755e67fe4e6f6830511
ShengLu <shenglu815@gmail.com>
2023-02-06 15:40:19 -0500

    broken merge code commented on line 98 to 122

--- shapefile/src/components/MapView.js
+++ shapefile/src/components/MapView.js
@@ -2,7 +2,10 @@ import React, {useState, useEffect, useRef} from 'react';
 import { MapContainer, TileLayer, useMap, GeoJSON, LayerGroup, FeatureGroup } from 'react-leaflet';
 import 'leaflet/dist/leaflet.css';
 import 'leaflet-draw/dist/leaflet.draw.css';
-import { GeomanControls } from 'react-leaflet-geoman-v2'
+import { GeomanControls } from 'react-leaflet-geoman-v2';
+// import { topojson } from 'topojson';
+import { topology } from 'topojson-server';
+import { merge as mergeRegion } from 'topojson-client';
 // import {GeomanJsWrapper} from './GeomanJsWrapper'
 
 function MapView(props) {
@@ -11,17 +14,20 @@ function MapView(props) {
     let regions = []
 
 
-    if(props.file.features){
-        console.log(props.file.features)
-    }
+    // if(props.file.features){
+    //     console.log(props.file.features)
+    // }
 
     const nameChange = (event) => {
         let layer = event.target;
         console.log(layer.feature.properties.name);
 
-        let newCountry = prompt("Input new region name:", layer.feature.properties.name);
-        props.changeName(layer.feature.properties.name, newCountry);
-        layer.bindPopup(newCountry)
+        let newName = prompt("Input new region name:", layer.feature.properties.name);
+        if(!newName){
+            return;
+        }
+        props.changeName(layer.feature.properties.name, newName);
+        layer.bindPopup(newName)
         layer.bindTooltip(layer.feature.properties.name,
             {permanent: true, direction:"center",className: "label"}
         ).openTooltip();
@@ -68,38 +74,77 @@ function MapView(props) {
         }
 
         //this will be our new combined region (we'll just add it to region1 ig
-        let combinedRegion = JSON.parse(JSON.stringify(region1))
+        let combinedRegion = region1
         combinedRegion.properties.name = region1.properties.name + region2.properties.name
 
-        let combinedCoords= combinedRegion.geometry.coordinates
-        let r2Coord= region2.geometry.coordinates
+        // let combinedCoords= combinedRegion.geometry.coordinates
+        // let region2Coord= region2.geometry.coordinates
 
+        // ===========================================================================
 
-        console.log(region2.geometry)
-        console.log(r2Coord)
-        console.log(r2Coord[0])
+        // let topoAll = topology(props.file.features);
+        // console.log("topo of whole", props.file, topoAll);
+        // let topoRegion1 = topology(region1);
+        // let topoRegion2 = topology(region2);
+        console.log("trying to merge", region1, region2);
+        // console.log("topoversion", topoRegion1, topoRegion2);
+        // let arc1 = {type: topoRegion1.objects.geometry.type, arcs: topoRegion1.arcs}
+        // let arc2 = {type: topoRegion2.objects.geometry.type, arcs: topoRegion2.arcs}
+        // console.log("arcs", arc1, arc2);
+        // let mergedRegion = mergeRegion(topoAll, [arc1, arc2]);
+        // console.log("topomerge", mergedRegion);
+        // console.log(topoRegion1);
+
+        // ===========================================================================
+
+        // console.log("whole", allRegionArray);
+        // let arr1 = [];
+        // for(let arr of region1.geometry.coordinates){
+        //     // console.log(arr)
+        //     arr1 = arr1.concat(arr[0]);
+        // }
+        // let arr2 = [];
+        // for(let arr of region2.geometry.coordinates){
+        //     // console.log(arr)
+        //     arr2 = arr2.concat(arr[0]);
+        // }
+
+        // let coord1String = arr1.map(JSON.stringify);
+        // let coord2String = arr2.map(JSON.stringify);
+        // // console.log(coord1String);
+        // let coordSetString = new Set(coord1String.concat(coord2String));
+        // let coordSet = Array.from(coordSetString, JSON.parse);
+        // console.log([coordSet]);
+
+        // combinedRegion.geometry.coordinates = [coordSet];
+        // combinedRegion.geometry.type = "Polygon";
+
+        // ===========================================================================
+
+        // console.log(region2)
+        // console.log(region2Coord)
+        // console.log(region2Coord[0])
         //TODO:
         //do the polygon magics here
         //merge by click on 2 country, and then click the merge button
         //if you run this without adding anything it will just delete the first region
         //and then create a new region with combined name
-        for(let polygons of r2Coord){
-            console.log("this is 1 polygon")
-            console.log(polygons)
-            //do something like combinedCoords.push(polygons) the idea is there but it just didnt work
-            for(let coords of polygons){
-                console.log("this is all of the 2d vec of that polygon")
-                console.log(coords)
-
-            }
-        }
+        // for(let polygons of region2Coord){
+        //     console.log("this is 1 polygon")
+        //     console.log(polygons)
+        //     //do something like combinedCoords.push(polygons) the idea is there but it just didnt work
+        //     console.log("this is all of the 2d vec of that polygon")
+        //     // for(let coords of polygons){
+        //     //     console.log(coords)
+        //     // }
+        // }
 
         //add the new combined region
         allRegionArray.push(combinedRegion)
         setUpdate(update+1) //absolutely crazy code but we need this to update the map
 
     }
-    const handleChange = (e) =>{
+    const handleCreate = (e) =>{
         console.log(e)
     }
 
@@ -136,10 +181,10 @@ function MapView(props) {
                                 globalOptions={{
                                     continueDrawing: true,
                                     editable: false,
-                                    limitMarkersToCount: 100,
+                                    limitMarkersToCount: 50,
                                     removeVertexOn: "contextmenu" //right click on verticies to remove
                                 }}
-                                onCreate={handleChange}
+                                onCreate={handleCreate}
                                 onChange={(e) => console.log('onChange', e)}
                             />
 
